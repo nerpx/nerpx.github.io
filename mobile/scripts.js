53611affb4dc6084a413769f398ae54100bb2d79
nerpx <cheb34119@gmail.com>
2026-04-10 22:52:24 -0400

    Add files via upload

---
+++ mobile/scripts.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const input = document.getElementById("number");
+
+  input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      pictureNumber();
+    }
+  });
+
+});
+
+function pictureNumber() {
+  const number = document.getElementById("number").value;
+  const pic = document.getElementById("pic");
+
+  const imagePath = "../media/image" + number + ".avif";
+
+  pic.onload = function () {
+    console.log("Picture " + number + " Displayed");
+  };
+
+  pic.onerror = function () {
+    console.log("Failed to load Picture " + number);
+    pic.onerror = null;
+    pic.onload = null;
+    pic.src = "../media/image4.avif";
+  };
+
+  pic.src = imagePath;
+}
