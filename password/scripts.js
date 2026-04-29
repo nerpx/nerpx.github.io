059e1de4cadf379d3daa505a771bdacebb4e9e56
nerpx <cheb34119@gmail.com>
2026-04-29 13:00:18 -0400

    Add files via upload

--- password/scripts.js
+++ password/scripts.js
@@ -1,17 +1,33 @@
+// a nerpx production
+
 let user1 = "admin";
 let pass1 = "nerpxinc";
 let user2 = "guest";
 let pass2 = "nerpxguest";
+let user3 = "eli";
+let pass3 = "eliisgay";
+
+function guest() {
+    document.getElementById("pw").setAttribute('type', 'text');
+    document.getElementById("un").value = "guest";
+    document.getElementById("pw").value = "nerpxguest";
+    setTimeout(() => {
+        textSubmit();
+      }, 2000);
+}
 
 function textSubmit() {
     let username = document.getElementById("un").value;
     let password = document.getElementById("pw").value;
-    if (username === user1 && password === pass1) {
+    if (
+      (username === user1 && password === pass1) ||
+      (username === user2 && password === pass2) ||
+      (username === user3 && password === pass3)
+    ) {
       document.getElementById("un").value = "Access Granted";
       document.getElementById("pw").value = "Access Granted";
       document.getElementById("button").innerText = "Access Granted";
       document.getElementById("button").disabled = true;
-      document.getElementById("body").style.backgroundColor = "black";
       setTimeout(() => {
         window.location.replace("https://nerpx.github.io");
       }, 2000);
@@ -20,13 +36,15 @@ function textSubmit() {
       document.getElementById("pw").value = "Access Denied";
       document.getElementById("button").innerText = "Access Denied";
       document.getElementById("button").disabled = true;
-      document.getElementById("body").style.backgroundColor = "#004040";
+      document.getElementById("logon").style.backgroundColor = "#489090";
+      document.getElementById("pw").setAttribute('type', 'text');
       setTimeout(() => {
         document.getElementById("un").value = "";
         document.getElementById("pw").value = "";
         document.getElementById("button").innerText = "Reattempt Entry";
         document.getElementById("button").disabled = false;
-        document.getElementById("body").style.backgroundColor = "#008080";
+        document.getElementById("logon").style.backgroundColor = "#189d9d";
+        document.getElementById("pw").setAttribute('type', 'password');
       }, 2000);
       
     }
