f9853478a9678640c5608ef59ed7afe0bb9d646a
nerpx <cheb34119@gmail.com>
2026-07-30 16:02:09 -0400

    Ok added more support

--- browtect.js
+++ browtect.js
@@ -1,11 +1,6 @@
-var HEADR =
-    "<h1>NerpX</h1>" +
-    "<p><i>for iOS 6</i></p>" +
-    '<a href="index.html" class="hlink"><i>Refresh Page</i></a>';
-
+/* This version stays compatible with Safari on iOS 6 and iOS 7. */
 
 var BODY =
-    
     '<div class="linkhead">' +
     '<h3>Wall Of Links</h3>' +
     '</div>' +
@@ -18,46 +13,51 @@ var BODY =
 
     '<br><br>' +
 
-    '<img src="Media/image4.jpg" class="mainimg" width="600" height="400"> <br><br>'
+    '<img src="Media/image4.jpg" class="mainimg" width="600" height="400"> <br><br>';
+
+
+function makeHeader(iosVersion) {
+    return (
+        '<h1>NerpX</h1>' +
+        '<p><i>for iOS ' + iosVersion + '</i></p>' +
+        '<a href="index.html" class="hlink"><i>Refresh Page</i></a>'
+    );
+}
 
 
 window.onload = function () {
-
     var ua = navigator.userAgent;
+    var isAppleMobile = /iPhone|iPad|iPod/.test(ua);
+    var ios = ua.match(/OS ([0-9]+)_/);
+    var major;
+    var header;
+    var main;
+    var css;
 
-    var isIPhone = ua.indexOf("iPhone") > -1;
-
-    var ios = ua.match(/OS ([0-9]+)_([0-9]+)/);
-
-
-    if (isIPhone && ios) {
-
-        var major = ios[1];
-
-
-        if (major == "6") {
-
-            var header = document.getElementById("header");
-            var main = document.getElementById("main");
-            var css = document.getElementById("stylesheet");
-
-
-            if (header != null) {
-                header.innerHTML = HEADR;
-            }
-
-
-            if (main != null) {
-                main.innerHTML = BODY;
-            }
-
-
-            if (css != null) {
-                css.href = "IOS6.css";
-            }
-
-        }
-
+    if (!isAppleMobile || !ios) {
+        return;
     }
 
+    major = parseInt(ios[1], 10);
+
+    // Run the legacy version on iOS 1 through iOS 7.
+    if (major >= 8) {
+        return;
+    }
+
+    header = document.getElementById("header");
+    main = document.getElementById("main");
+    css = document.getElementById("stylesheet");
+
+    if (header !== null) {
+        header.innerHTML = makeHeader(major);
+    }
+
+    if (main !== null) {
+        main.innerHTML = BODY;
+    }
+
+    if (css !== null) {
+        css.href = "IOS6.css";
+    }
 };
--- scripts.js
+++ scripts.js
@@ -1,31 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  const input = document.getElementById("number");
-
-  input.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      pictureNumber();
-    }
-  });
-
-});
+/* Image-number picker for the main site. */
 
 function pictureNumber() {
-  const number = document.getElementById("number").value;
-  const pic = document.getElementById("pic");
+    var input = document.getElementById("number");
+    var picture = document.getElementById("pic");
+    var imageNumber;
+    var imagePath;
 
-  const imagePath = "Media/image" + number + ".avif";
+    // This page does not include the picker on legacy iOS layouts.
+    if (input === null || picture === null) {
+        return;
+    }
 
-  pic.onload = function () {
-    console.log("Picture " + number + " Displayed");
-  };
+    imageNumber = input.value;
 
-  pic.onerror = function () {
-    console.log("Failed to load Picture " + number);
-    pic.onerror = null;
-    pic.onload = null;
-    pic.src = "Media/image4.avif";
-  };
+    // Only accept whole, positive image numbers such as 4 or 26.
+    if (!/^[1-9][0-9]*$/.test(imageNumber)) {
+        picture.src = "Media/image4.avif";
+        return;
+    }
 
-  pic.src = imagePath;
+    imagePath = "Media/image" + imageNumber + ".avif";
+
+    picture.onload = function () {
+        picture.onload = null;
+        picture.onerror = null;
+        console.log("Picture " + imageNumber + " displayed");
+    };
+
+    picture.onerror = function () {
+        picture.onerror = null;
+        picture.onload = null;
+        picture.src = "Media/image4.avif";
+        console.log("Picture " + imageNumber + " was not found; showing picture 4");
+    };
+
+    picture.src = imagePath;
 }
+
+function setUpPicturePicker() {
+    var input = document.getElementById("number");
+
+    if (input === null) {
+        return;
+    }
+
+    input.addEventListener("keydown", function (event) {
+        if (event.key === "Enter" || event.keyCode === 13) {
+            pictureNumber();
+        }
+    }, false);
+}
+
+document.addEventListener("DOMContentLoaded", setUpPicturePicker, false);
