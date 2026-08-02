71c4ca636615e0a2306826173a7e92fb5427120e
nerpx <cheb34119@gmail.com>
2026-08-01 22:52:06 -0400

    DEATH

--- Search/scripts.js
+++
@@ -1,5 +0,0 @@
-function changeIframeSrc() {
-            var newUrl = document.getElementById('urlInput').value;
-            var iframeElement = document.getElementById('myIframe');
-            iframeElement.src = newUrl;
-        }
